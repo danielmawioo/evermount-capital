8ffcd76a3fea2b4436534f367429663f0fe89fe0
danielmawioo <workd1312@gmail.com>
2025-04-27 11:59:34 +0300

    update

--- src/app/dashboard/deposit/method/page.tsx
+++ src/app/dashboard/deposit/method/page.tsx
@@ -1,7 +1,8 @@
 // Deposit Payment Selection Page
 "use client";
 
-import PaymentMethodsPage from "../../components/PaymentMethodsPage.tsx";
+import PaymentMethodsPage from "../../components/PaymentMethodsPage";
+
 
 export default function DepositMethodPage() {
   return <PaymentMethodsPage type="deposit" />;
